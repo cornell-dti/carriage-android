e9efb452986839f2d7e7bde2c38192ed67010695
Tony Yang <tony.yang98@gmail.com>
2019-10-29 18:18:18 -0400

    implemented bottom navigation drawer (#5)
    * Update README.md
    
    * set up gradle
    
    * organize directory
    
    * insert .gitignore into Rider
    
    * Update .gitignore
    
    * remove .DS_Store
    
    * Remove junk
    
    * create bottom navigation bar
    
    * change file location

--- Rider/app/src/main/java/com/example/rider/MainActivity.kt
+++ Rider/app/src/main/java/com/example/rider/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.example.rider
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainFragment: MainFragment
+    lateinit var secondFragment: SecondFragment
+    lateinit var thirdFragment: ThirdFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        mainFragment = MainFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout, mainFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+
+        bottomNavigation.setOnNavigationItemSelectedListener{ item ->
+
+            when (item.itemId) {
+                R.id.navigationOne -> {
+                    mainFragment = MainFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, mainFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.navigationTwo -> {
+                    secondFragment = SecondFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, secondFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.navigationThree -> {
+                    thirdFragment = ThirdFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, thirdFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+
+            }
+
+            true
+        }
     }
+
 }
