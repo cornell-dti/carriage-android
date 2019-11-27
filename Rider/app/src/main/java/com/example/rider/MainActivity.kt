089cc132ae79c0f8adfa1f12be1700df580c56ac
Jessie <jw823@cornell.edu>
2019-11-26 21:10:21 -0500

    Home Page
    Finished first draft of Home Page of the Driver Side of App.

--- Rider/app/src/main/java/com/example/rider/MainActivity.kt
+++ Rider/app/src/main/java/com/example/rider/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.rider
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.upcoming_rides.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+    }
 }
